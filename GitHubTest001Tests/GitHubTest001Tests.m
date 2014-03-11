e09cfe28a0438ffb1e6c77304cdca4b1c14ca756
user <user@MTPC4031ui-iMac.local>
2014-03-11 14:16:37 +0900

    Initial Commit

---
+++ GitHubTest001Tests/GitHubTest001Tests.m
@@ -0,0 +1,34 @@
+//
+//  GitHubTest001Tests.m
+//  GitHubTest001Tests
+//
+//  Created by user on 14. 3. 11..
+//  Copyright (c) 2014년 user. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GitHubTest001Tests : XCTestCase
+
+@end
+
+@implementation GitHubTest001Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
